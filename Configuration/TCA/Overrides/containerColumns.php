319179a63d3cb13bf4368e54ae182f4faf867192
Tanel Põld <tanel@brightside.ee>
2021-12-17 17:56:09 +0200

    [FEATURE] Add 4 column layout

--- Configuration/TCA/Overrides/containerColumns.php
+++ Configuration/TCA/Overrides/containerColumns.php
@@ -4,7 +4,7 @@
     (
         new \B13\Container\Tca\ContainerConfiguration(
             'containerColumns-1-1',
-            'Two Columns 50/50',
+            '2 Columns 50/50',
             'Dividing the content area into two equal columns',
             [
                 [
@@ -22,7 +22,7 @@
     (
         new \B13\Container\Tca\ContainerConfiguration(
             'containerColumns-2-1',
-            'Two Columns 60/40',
+            '2 Columns 60/40',
             'Dividing the content area into two columns with left column being wider',
             [
                 [
@@ -39,7 +39,7 @@
     (
         new \B13\Container\Tca\ContainerConfiguration(
             'containerColumns-1-2',
-            'Two Columns 40/60',
+            '2 Columns 40/60',
             'Dividing the content area into two columns with right column being wider',
             [
                 [
@@ -56,12 +56,31 @@
     (
         new \B13\Container\Tca\ContainerConfiguration(
             'containerColumns-1-1-1',
-            'Three Columns 33/33/33',
+            '3 Columns 33/33/33',
             'Dividing the content area into three equal columns',
             [
                 [
                     ['name' => 'Left', 'colPos' => 101],
                     ['name' => 'Middle', 'colPos' => 102],
+                    ['name' => 'Right', 'colPos' => 103]
+                ]
+            ]
+        )
+    )
+    ->setIcon('EXT:container/Resources/Public/Icons/container-3col.svg')
+);
+
+\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\B13\Container\Tca\Registry::class)-> configureContainer(
+    (
+        new \B13\Container\Tca\ContainerConfiguration(
+            'containerColumns-1-1-1-1',
+            '4 Columns 25/25/25/25',
+            'Dividing the content area into four equal columns',
+            [
+                [
+                    ['name' => 'Left', 'colPos' => 101],
+                    ['name' => 'Middle Left', 'colPos' => 102],
+                    ['name' => 'Middle Right', 'colPos' => 103],
                     ['name' => 'Right', 'colPos' => 103]
                 ]
             ]
