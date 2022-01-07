bea1f397d8644b46b7f20ea96cbe6664f341e0f1
Tanel Põld <tanel@brightside.ee>
2022-01-07 12:00:27 +0200

    [RELEASE] 2.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
     'title' => 'Containeritems',
     'description' => 'EXT:container layouts for columns, accordions and content sections. Demo at: https://microtemplate.t3brightside.com',
     'category' => 'fe',
-    'version' => '1.1.0',
+    'version' => '2.0.0',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
     'author' => 'Tanel Põld',
