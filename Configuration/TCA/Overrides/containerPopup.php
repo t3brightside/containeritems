f368bf712c1dbd5ff7899daadf6bee676f4f9b21
Tanel Põld <tanel@brightside.ee>
2023-03-15 21:28:50 +0200

    [TASK] Popup container

---
+++ Configuration/TCA/Overrides/containerPopup.php
@@ -0,0 +1,24 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\B13\Container\Tca\Registry::class)-> configureContainer(
+    (
+        new \B13\Container\Tca\ContainerConfiguration(
+            'containerPopup',
+            'Popup',
+            'Hidden by default container for popups',
+            [
+                [
+                    ['name' => 'Popup', 'colPos' => 101],
+                ]
+            ]
+        )
+    )
+    ->setIcon('EXT:containeritems/Resources/Public/Icons/Containers/containerGather.svg')
+);
+
+$GLOBALS['TCA']['tt_content']['types']['containerPopup']['showitem'] = str_replace(
+    'header;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header.ALT.div_formlabel,',
+    'header;Title (shown in special cases only),header_layout,
+    tx_containeritems_classes,tx_containeritems_customid,',
+    $GLOBALS['TCA']['tt_content']['types']['containerPopup']['showitem']
+);
