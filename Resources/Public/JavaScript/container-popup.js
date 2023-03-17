e04c81a2d577062df82ff85580f05ca8605b66b3
Tanel Põld <tanel@brightside.ee>
2023-03-17 06:47:21 +0200

    [TASK] Popup z-index fix and contentWidth

--- Resources/Public/JavaScript/container-popup.js
+++ Resources/Public/JavaScript/container-popup.js
@@ -1,20 +1,20 @@
 const popupOpeners = document.querySelectorAll('.c-popup-opener');
 
 popupOpeners.forEach(button => {
-    button.addEventListener('mouseup', () => {
-        const openButtonName = event.target.name;
-        document.getElementById(openButtonName).classList.add('open');
-        document.body.style.overflow = 'hidden';
-    });
+  button.addEventListener('mouseup', () => {
+    const openButtonName = event.target.name;
+    document.getElementById(openButtonName).classList.add('open');
+    document.body.style.overflow = 'hidden';
+  });
 });
 
 const popupCloses = document.querySelectorAll('.c-popup-close');
 popupCloses.forEach(button => {
-    button.addEventListener('mouseup', () => {
-        const closeButtonName = event.target.name;
-        document.getElementById(closeButtonName).classList.remove('open');
-        if (!document.querySelectorAll('.c-popup.open').length) {
-        document.body.style.overflow = 'scroll';
-        }
-    });
+  button.addEventListener('mouseup', () => {
+    const closeButtonName = event.target.name;
+    document.getElementById(closeButtonName).classList.remove('open');
+    if (!document.querySelectorAll('.c-popup.open').length) {
+      document.body.style.overflow = 'scroll';
+    }
+  });
 });
