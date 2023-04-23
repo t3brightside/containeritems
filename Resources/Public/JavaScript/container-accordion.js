e08877b35fa4f4ba578f1ed9025a3aefc8f9e39a
Tanel Põld <tanel@brightside.ee>
2023-04-23 17:47:58 +0300

    [BUGFIX] Allow multiple accordions on same page

--- Resources/Public/JavaScript/container-accordion.js
+++ Resources/Public/JavaScript/container-accordion.js
@@ -1,27 +1,28 @@
-const accordionContainer = document.querySelector('.c-accordion');
-
-accordionContainer.addEventListener('click', event => {
-  const target = event.target;
-  if (target.classList.contains('c-accordion-title')) {
-    const accordionItem = target.parentElement;
-    let el = accordionItem.querySelector('.c-accordion-content');
-    let height = el.scrollHeight;
-    el.style.setProperty('--max-height', height + 'px');
-    if (accordionItem.classList.contains('stayopen-1')) {
-      accordionItem.classList.toggle('act');
-    }
-    else {
-      if (accordionItem.classList.contains('act')) {
-        Array.from(accordionItem.parentElement.querySelectorAll('.act')).forEach(function(el) {
-          el.classList.remove('act');
-        });
-      }
-      else {
-        Array.from(accordionItem.parentElement.querySelectorAll('.act')).forEach(function(el) {
-          el.classList.remove('act');
-        });
+const accordions = document.querySelectorAll('.c-accordion');
+accordions.forEach(item => {
+  item.addEventListener('click', event => {
+    const target = event.target;
+    if (target.classList.contains('c-accordion-title')) {
+      const accordionItem = target.parentElement;
+      let el = accordionItem.querySelector('.c-accordion-content');
+      let height = el.scrollHeight;
+      el.style.setProperty('--max-height', height + 'px');
+      if (accordionItem.classList.contains('stayopen-1')) {
         accordionItem.classList.toggle('act');
       }
+      else {
+        if (accordionItem.classList.contains('act')) {
+          Array.from(accordionItem.parentElement.querySelectorAll('.act')).forEach(function(el) {
+            el.classList.remove('act');
+          });
+        }
+        else {
+          Array.from(accordionItem.parentElement.querySelectorAll('.act')).forEach(function(el) {
+            el.classList.remove('act');
+          });
+          accordionItem.classList.toggle('act');
+        }
+      }
     }
-  }
+  })
 });
