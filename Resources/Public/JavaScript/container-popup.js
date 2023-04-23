bb21d57f319158cc0cbae7bd8187ee694e7f296e
Tanel Põld <tanel@brightside.ee>
2023-04-23 23:03:45 +0300

    [FEATURE] add JS to open popups with anchor links

--- Resources/Public/JavaScript/container-popup.js
+++ Resources/Public/JavaScript/container-popup.js
@@ -18,3 +18,29 @@ popupCloses.forEach(button => {
     }
   });
 });
+
+
+const allanchors = document.getElementsByTagName('a');
+
+// Loop through the anchor elements and add a click event listener to the ones with an href containing "#"
+for (let i = 0; i < allanchors.length; i++) {
+  const href = allanchors[i].getAttribute('href');
+  if (href && href.indexOf('#') !== -1) {
+    allanchors[i].addEventListener('click', function(event) {
+      // Get the target element with the same ID as the anchor link's href
+      const targetId = href.substring(href.indexOf('#') + 1);
+      const target = document.getElementById(targetId);
+
+      // Check if a target element was found and add the "my-class" class to it if it has the "c-popup" class
+      if (target && target.classList.contains('c-popup')) {
+        // Prevent the default behavior of the anchor link
+        document.body.style.overflow = 'hidden';
+
+        event.preventDefault();
+
+        // Add the "my-class" class to the target element
+        target.classList.add('open');
+      }
+    });
+  }
+}
