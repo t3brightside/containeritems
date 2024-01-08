037e564d88fa113961ab5ac55ea2554fe8f50e29
Tanel Põld <tanel@brightside.ee>
2024-01-08 22:58:37 +0200

    [TASK] add custom classes and improve accessibility

--- Resources/Public/JavaScript/container-accordion.js
+++ Resources/Public/JavaScript/container-accordion.js
@@ -1,28 +1,29 @@
-const accordions = document.querySelectorAll('.c-accordion');
-accordions.forEach(item => {
-  item.addEventListener('click', event => {
-    const target = event.target;
-    if (target.classList.contains('c-accordion-title')) {
-      const accordionItem = target.parentElement;
-      let el = accordionItem.querySelector('.c-accordion-content');
-      let height = el.scrollHeight;
-      el.style.setProperty('--max-height', height + 'px');
-      if (accordionItem.classList.contains('stayopen-1')) {
-        accordionItem.classList.toggle('act');
-      }
-      else {
-        if (accordionItem.classList.contains('act')) {
-          Array.from(accordionItem.parentElement.querySelectorAll('.act')).forEach(function(el) {
-            el.classList.remove('act');
-          });
+const accordionContainers = document.querySelectorAll('.c-accordion');
+
+accordionContainers.forEach(item => {
+    item.addEventListener('click', event => {
+        const target = event.target;
+        if (target.classList.contains('c-accordion-title')) {
+            const accordionItem = target.parentElement;
+            let el = accordionItem.querySelector('.c-accordion-content');
+            let height = el.scrollHeight;
+            el.style.setProperty('--max-height', height + 'px');
+
+            const isStayOpen = item.classList.contains('stayopen');
+            const isAccordionItemActive = accordionItem.classList.contains('act');
+
+            // Toggle 'act' class on accordionItem based on whether it stays open or not
+            if (isStayOpen) {
+                accordionItem.classList.toggle('act');
+            } else {
+                Array.from(accordionItem.parentElement.querySelectorAll('.act')).forEach(el => {
+                    el.classList.remove('act');
+                });
+                accordionItem.classList.toggle('act');
+            }
+
+            // Toggle aria-expanded attribute on the title
+            target.setAttribute('aria-expanded', isAccordionItemActive ? 'false' : 'true');
         }
-        else {
-          Array.from(accordionItem.parentElement.querySelectorAll('.act')).forEach(function(el) {
-            el.classList.remove('act');
-          });
-          accordionItem.classList.toggle('act');
-        }
-      }
-    }
-  })
+    });
 });
