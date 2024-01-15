98bb530dded081baccfefc816e1f47c1f11af85a
Tanel Põld <tanel@brightside.ee>
2024-01-15 07:27:10 +0200

    [TASK] Add aria-selected and aria-expanded functions to tabs

--- Resources/Public/JavaScript/container-tabs.js
+++ Resources/Public/JavaScript/container-tabs.js
@@ -3,21 +3,27 @@ document.addEventListener('DOMContentLoaded', function () {
   const tabsContainers = document.querySelectorAll('.frame-container-tabs');
 
   tabsContainers.forEach(tabsContainer => {
-      // Get tab items and content within each tabs container
-      const tabTitles = tabsContainer.querySelectorAll('.c-tabs-title');
-      const tabContents = tabsContainer.querySelectorAll('.c-tabs-content');
+    // Get tab items and content within each tabs container
+    const tabTitles = tabsContainer.querySelectorAll('.c-tabs-title');
+    const tabContents = tabsContainer.querySelectorAll('.c-tabs-content');
 
-      // Add click event listener to each tab within the current tabs container
-      tabTitles.forEach((tab, index) => {
-          tab.addEventListener('click', function () {
-              // Remove active class from all tabs and contents within the current tabs container
-              tabTitles.forEach(title => title.classList.remove('act'));
-              tabContents.forEach(content => content.classList.remove('act'));
+    // Add click event listener to each tab within the current tabs container
+    tabTitles.forEach((tab, index) => {
+      tab.addEventListener('click', function () {
+        // Remove active class from all tabs and contents within the current tabs container
+        tabTitles.forEach(title => {
+          title.classList.remove('act');
+          title.setAttribute('aria-selected', 'false');
+          title.setAttribute('aria-expanded', 'false');
+        });
+        tabContents.forEach(content => content.classList.remove('act'));
 
-              // Add active class to the clicked tab and corresponding content within the current tabs container
-              this.classList.add('act');
-              tabContents[index].classList.add('act');
-          });
+        // Add active class and aria attributes to the clicked tab and corresponding content within the current tabs container
+        this.classList.add('act');
+        this.setAttribute('aria-selected', 'true');
+        this.setAttribute('aria-expanded', 'true');
+        tabContents[index].classList.add('act');
       });
+    });
   });
 });
