08a8fbd3e45877a56ef5ef6fc08efcb6514c0132
Tanel Põld <tanel@brightside.ee>
2026-05-24 21:16:18 +0300

    [RELEASE] 6.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
     'title' => 'Containeritems',
     'description' => 'EXT:container layouts for columns, accordions, carousels, content sections and boxes. Demo at: https://microtemplate.t3brightside.com',
     'category' => 'fe',
-    'version' => '5.0.0',
+    'version' => '6.0.0',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
     'author' => 'Tanel Põld',
@@ -11,10 +11,9 @@
     'author_company' => 'Brightside OÜ',
     'constraints' => [
       'depends' => [
-        'typo3' => '11.5.0-13.99.99',
-        'fluid_styled_content' => '11.5.0-14.99.99',
-        'container' => '2.3.0-3.99.99',
-        'embedassets' => '1.2.0-1.5.99',
+        'typo3' => '13.4.0-14.99.99',
+        'container' => '3.0.0-3.99.99',
+        'embedassets' => '1.4.0-1.99.99',
       ],
     ],
   ];
